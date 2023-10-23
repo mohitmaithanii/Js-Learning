c471b43d2b8a9365dc1fdc9b0c2b34ad4118a370
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-24 04:51:05 +0530

    first commit

---
+++ 01_vriables.js
@@ -0,0 +1,8 @@
+const accountId = 144552
+let accountEmail = "mohitmaithani@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountId = 2 //not allowed
+
+console.log(accountId);
