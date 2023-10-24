ec6eaa494ed467b1b2391632f853dfe0c6585b7e
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-24 05:35:13 +0530

    end repository

--- 01_vriables.js
+++ 01_vriables.js
@@ -1,8 +1,37 @@
-const accountId = 144552
-let accountEmail = "mohitmaithani@gmail.com"
-var accountPassword = "12345"
-accountCity = "Delhi"
+//! const , let , var
 
-// accountId = 2 //not allowed
+const accountId = 144552;
+/* const use for declare constance in js */
+
+let accountEmail = "abc@gmail.com";
+/* let and var declare variables in Js */
+
+var accountPassword = "12345";
+/*  prefer not to use var 
+because of issue in block scope and functional scope */
+
+accountCity = "Delhi";
+/* in Js this is also way to declare variables 
+but don't use it */
+
+let accountState;
+/* variable without value declaration */
+
+// accountId = 2 // same const not use again and not change
+accountEmail = "xyz@gmail.com";
+accountPassword = "654321";
+accountCity = "Pune";
+accountState = "UK"
+
 
 console.log(accountId);
+/* use for print value */
+
+console.table([
+   accountId,
+   accountEmail,
+   accountPassword,
+   accountCity,
+   accountState,
+]);
+//all variables print in table form with index no.
