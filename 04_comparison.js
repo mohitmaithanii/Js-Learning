9bcdc585e7af9a06e5393abbb8eb2b1760a3bfd7
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-26 04:36:34 +0530

    datatype comparison

--- 04_comparison.js
+++ 04_comparison.js
@@ -10,7 +10,18 @@ console.log(2 <= 1); //false
 console.log(2 == 1); //false
 console.log(2 != 1); //true
 
+// deferent datatypes comparison =>
 
+// avoid this type of code and don't use 
 
+console.log("2" > 1); //true
+console.log("02" > 1); //true
 
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
