ed933fe3d411b2851a3369d18670bc385efbf0f1
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-25 07:46:51 +0530

    operators

--- 03_conversion_operation.js
+++ 03_conversion_operation.js
@@ -11,7 +11,6 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber); // Number
 console.log(valueInNumber); // print value = 33
 
-
 // example 2=>
 let value = null;
 
@@ -29,7 +28,6 @@ undefined => NaN
 null => 0
 */
 
-
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
@@ -42,11 +40,61 @@ console.log(booleanIsLoggedIn);
 "mohit" => true
 */
 
-
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber); // string
 
+// Operations =>
 
+let val = 3;
+let negValue = -val;
+console.log(negValue); // -3
+
+/* 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); 
+*/
+
+let str1 = "hello";
+let srt2 = " mohit";
+
+let str3 = str1 + srt2;
+console.log(str3);
+
+/* 
+don't use these type of tricky operations =>
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 + "2");
+
+console.log(((3 + 4) * 5) % 3);
+console.log(+true);
+console.log(+"");
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+*/
+
+/* link to study =>
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+//increment operator
+let gameCounterInc = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//decrement operator
+let gameCounterDec = 100;
+gameCounter--;
+--gameCounter;
+console.log(gameCounter);
