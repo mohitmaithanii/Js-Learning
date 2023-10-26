6efa1814d909261fdc84568042d3a825d606897d
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-27 04:57:36 +0530

    some strings methods

--- basics-of-js/07_strings.js
+++ basics-of-js/07_strings.js
@@ -7,3 +7,34 @@ console.log(name + repoCount + " Value"); // don't use
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 // string interpolation
+
+const gameName = new String("mohit.com");
+
+console.log(gameName[0]); // key value pair
+console.log(gameName.__proto__); // prototypes methods
+
+//some strings methods
+console.log(gameName.length); // 5
+console.log(gameName.toUpperCase()); //MOHIT
+console.log(gameName.charAt(2)); // h
+console.log(gameName.indexOf("t")); //4
+
+const newString = gameName.substring(0, 3);
+console.log(newString); // moh
+
+const anotherString = gameName.slice(0, 3);
+console.log(anotherString); // moh
+
+const newString1 = "    mohit    ";
+console.log(newString1); //     mohit
+console.log(newString1.trim()); // mohit
+
+const url = "https://google.com/xyz%12abc";
+console.log(url.replace("%12", "-")); // https://google.com/xyz-abc
+console.log(url.includes("abc")); // true
+console.log(url.split("/")); // [ 'https:', '', 'google.com', 'xyz%12abc' ]
+
+/* 
+Study link =>
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String */
