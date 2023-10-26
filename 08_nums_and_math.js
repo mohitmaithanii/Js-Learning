7eb8a5158efcd5bfe9be74e396dc1eb6d086a747
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-27 05:15:40 +0530

    Maths

--- 08_nums_and_math.js
+++ 08_nums_and_math.js
@@ -1,5 +1,18 @@
-const score = 400;
-console.log(score); // 400
+const score = 100;
+console.log(score); // 100
 
 const balance = new Number(100);
 console.log(balance); //[Number: 100]
+
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2)); // 100.00
+
+const otherNumber = 213.2321;
+console.log(otherNumber.toPrecision(4)); //213.2
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString()); // 10,00,000
+
+// Maths =>
+
+console.log(Math); //Object [Math] {}
