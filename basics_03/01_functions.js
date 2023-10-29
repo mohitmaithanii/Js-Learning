11e0ab3224e5df2bce2ae12691b218bbaff60db9
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 05:20:52 +0530

    notes update

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -53,47 +53,6 @@ function loginUserMessage(username) {
 }
 console.log(loginUserMessage());
 
-//rest operator ->
-function calculateCratePrice(num1) {
-   return num1;
-}
-console.log(calculateCratePrice(200, 400, 500, 2000)); //200
-
-function calculateCratePrice(...num1) {
-   return num1;
-}
-console.log(calculateCratePrice(200, 400, 500, 2000)); //[ 200, 400, 500, 2000 ]
-
-function calculateCratePrice(val1, val2, ...num1) {
-   return num1;
-}
-console.log(calculateCratePrice(200, 400, 500, 2000)); //[ 500, 2000 ]
-
-// function with object ->
-const user = {
-   username: "mohit",
-   age: 21,
-};
-function handleObject(anyobject) {
-   console.log(
-      `username is ${anyobject.username} and price is ${anyobject.age}`
-   );
-}
-handleObject(user);
-
-handleObject({
-   username: "sam",
-   age: 18,
-});
-
-// function with array ->
-const myNewArray = [200, 400, 100, 600];
-
-function returnSecondValue(getArray) {
-   return getArray[1];
-}
-console.log(returnSecondValue(myNewArray));
-
 /* 
 study link =>
 
