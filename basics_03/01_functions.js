8f802e2dc02e1f12f481d5ca4bb3171e07ef55e7
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 05:08:33 +0530

    rest operator

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -53,6 +53,23 @@ function loginUserMessage(username) {
 }
 console.log(loginUserMessage());
 
+
+//rest operator ->
+function calculateCratePrice(num1) {
+   return num1;
+}
+console.log(calculateCratePrice(200, 400, 500, 2000)); //200
+
+function calculateCratePrice(...num1) {
+   return num1;
+}
+console.log(calculateCratePrice(200, 400, 500, 2000)); //[ 200, 400, 500, 2000 ]
+
+function calculateCratePrice(val1, val2, ...num1) {
+   return num1;
+}
+console.log(calculateCratePrice(200, 400, 500, 2000)); //[ 500, 2000 ]
+
 /* 
 study link =>
 
