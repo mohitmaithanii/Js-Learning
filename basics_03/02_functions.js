71c27945ba039f60b25423f9b16e0173148ce1d9
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 05:23:04 +0530

    functions with array

--- basics_03/02_functions.js
+++ basics_03/02_functions.js
@@ -31,3 +31,10 @@ handleObject({
    age: 18,
 });
 
+// function with array ->
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+   return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
