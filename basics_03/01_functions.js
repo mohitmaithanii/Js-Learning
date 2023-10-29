cada3066bbf7e9a05fead85a7f6eb8c5e0e7693d
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 04:57:36 +0530

    notes update

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -1,4 +1,4 @@
-//function => a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output.
+//function => a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output.
 
 function sayMyName() {
    console.log("m");
