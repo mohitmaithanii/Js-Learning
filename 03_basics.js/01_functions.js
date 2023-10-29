9eb6b6f8175bb4518fbc96e6247a8cf0f2fd59af
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 04:42:00 +0530

    parameters

---
+++ 03_basics.js/01_functions.js
@@ -0,0 +1,32 @@
+//function =>
+
+function sayMyName() {
+   console.log("m");
+   console.log("o");
+   console.log("h");
+   console.log("i");
+   console.log("t");
+}
+//sayMyName();
+
+function addTwoNumbers(num1, num2) {
+   //(parameter)
+   console.log(num1 + num2);
+}
+
+addTwoNumbers(3, 4); //arguments
+
+function addTwoNumbers(num1, num2) {
+   //let result = num1 + num2;
+   //return result;
+   return num1 + num2;
+}
+
+const result = addTwoNumbers(3, 5);
+
+console.log("result:", result); //result: 8
+
+function loginUserMessage(username) {
+   return `${username} just logged in`;
+}
+console.log(loginUserMessage("mohit")); //mohit just logged in
