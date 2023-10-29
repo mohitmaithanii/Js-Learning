debfdb1219c45f0872e6ffa60241607be74b6526
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 04:48:10 +0530

    if statement in functions

--- 03_basics.js/01_functions.js
+++ 03_basics.js/01_functions.js
@@ -7,15 +7,15 @@ function sayMyName() {
    console.log("i");
    console.log("t");
 }
-sayMyName();
+//sayMyName();
 
-function addTwoNumbers(num1, num2) {
+/* function addTwoNumbers(num1, num2) {
     //(parameter)
    console.log(num1 + num2);
 }
 
 addTwoNumbers(3, 4); //arguments 
-
+ */
 
 function addTwoNumbers(num1, num2) {
    //let result = num1 + num2;
@@ -25,16 +25,31 @@ function addTwoNumbers(num1, num2) {
 
 const result = addTwoNumbers(3, 5);
 
-console.log("result:", result); //result: 8
+//console.log("result:", result); //result: 8
 
 function loginUserMessage(username) {
    return `${username} just logged in`;
 }
-console.log(loginUserMessage("mohit")); //mohit just logged in
+//console.log(loginUserMessage("mohit")); //mohit just logged in
 
-function loginUserMessage(username = "sam") {
-   //default parameters
+function loginUserMessage(username) {
    return `${username} just logged in`;
 }
-console.log(loginUserMessage()); //sam just logged in
+//console.log(loginUserMessage()); //undefined just logged in
 
+//default parameters
+function loginUserMessage(username = "sam") {
+   return `${username} just logged in`;
+}
+//console.log(loginUserMessage()); //sam just logged in
+
+//if statement
+function loginUserMessage(username) {
+   //if (username === undefined)
+   if (!username) {
+      console.log("Please enter a username");
+      return;
+   }
+   return `${username} just logged in`;
+}
+console.log(loginUserMessage());
