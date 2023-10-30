659faa5f659a14f4c553e17c56cfcbd0e8ec6a03
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-30 05:54:02 +0530

    notes updates

--- basics_03/03_scopes.js
+++ basics_03/03_scopes.js
@@ -19,3 +19,37 @@ if (true) {
 console.log(a); //300
 console.log(b); //400
 console.log(c); //30 avoid var
+
+//nested functions ->
+function one() {
+   const username = "mohit";
+
+   function two() {
+      const website = "youtube";
+      console.log(username); //mohit
+   }
+   console.log(website); //not defined
+   two();
+}
+one();
+
+if (true) {
+   const username = "mohit";
+   if (username === "mohit") {
+      const website = " youtube";
+      console.log(username + website); //mohit youtube
+   }
+   console.log(website); //not defined
+}
+console.log(username); //not defined
+
+// interesting ->
+console.log(addOne(5)); //6
+function addOne(num) {
+   return num + 1;
+}
+
+console.log(addTwo(5)); //Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+   return num + 2;
+};
