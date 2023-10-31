1aef55bec0732f0ef17b8e58588dcb04064991b1
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-31 07:24:56 +0530

    named / unnamed IIFE

--- basics_03/05_iife.js
+++ basics_03/05_iife.js
@@ -1,5 +1,15 @@
 // Immediately Invoked Function Expressions
 
 (function func() {
+   //named IIFE
    console.log("DB connected");
-})();
+})(); //DB connected
+
+(() => {
+   console.log("DB connected 2 ");
+})(); //DB connected 2
+
+((name) => {
+   //unnamed IIFE /simple
+   console.log(`DB connected 2 ${name} `);
+})("mohit"); //DB connected 2 mohit
