0cac05fcda0d17ac53d9f228ca0d659a2d34491a
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-10-31 06:58:22 +0530

    this

--- basics_03/04_arrow.js
+++ basics_03/04_arrow.js
@@ -4,9 +4,34 @@ const user = {
 
    welcomeMessage: function () {
       console.log(`${this.username} , welcome to website`);
+      console.log(this);
    },
 };
 
-user.welcomeMessage();
-user.username = "sam";
-user.welcomeMessage();
+//user.welcomeMessage();
+//user.username = "sam";
+//user.welcomeMessage();
+
+//console.log(this); //{}
+
+/* function fun() {
+   let username = "mohit";
+   console.log(this.username);
+}
+fun(); //undefined 
+*/
+
+/* const fun2 = function () {
+   let username = "mohit";
+   console.log(this.username);
+};
+
+fun2(); //undefined
+ */
+
+/* const fun3 = () => {
+   let username = "mohit";
+   console.log(this.username);
+};
+fun3(); //undefined
+ */
