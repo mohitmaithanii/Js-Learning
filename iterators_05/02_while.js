7c2529a9bb9241c24351cb33a70e928d0bbd3712
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-02 05:20:17 +0530

    update

---
+++ iterators_05/02_while.js
@@ -0,0 +1,15 @@
+// while loop =>
+
+let i = 0;
+while (i <= 10) {
+   console.log(`Value of index ${i}`);
+   i++;
+}
+
+let myArr = ["flash", "batman", "superman"];
+
+let arr = 0;
+while (arr < myArr.length) {
+   console.log(`Value is ${myArr[arr]}`);
+   arr++;
+}
