daad8f547ca154648f26646acf6f2134c9425ff8
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-01 05:45:46 +0530

    notes update

--- control_flow_04/02_switch.js
+++ control_flow_04/02_switch.js
@@ -1,10 +1,30 @@
 // Switch =>
 
 switch (key) {
-    case value:
-        
-        break;
+   case value:
+      break;
 
-    default:
-        break;
+   default:
+      break;
+}
+
+const month = 3;
+
+switch (month) {
+   case 1:
+      console.log("January");
+      break;
+   case 2:
+      console.log("Fab");
+      break;
+   case 3:
+      console.log("March");
+      break;
+   case 4:
+      console.log("April");
+      break;
+
+   default:
+      console.log("default case");
+      break;
 }
