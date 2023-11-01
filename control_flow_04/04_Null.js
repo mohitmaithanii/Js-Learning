8be6f0ac2a1dcb31aad8f0b69045c1bca054d20c
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-01 06:15:18 +0530

    Ternary Operator

--- control_flow_04/04_Null.js
+++ control_flow_04/04_Null.js
@@ -1,1 +1,15 @@
 // Nullish Coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10; // 5
+val1 = null ?? 10; // 10
+val1 = undefined ?? 15; // 15
+val1 = null ?? 10 ?? 20; // 10
+
+console.log(val1);
+
+// Ternary Operator =>
+
+/* condition ? true : false */
+const teaPrice = 100;
+teaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
