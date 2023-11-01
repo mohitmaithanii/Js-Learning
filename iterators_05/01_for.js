c9dd89e634f7db7284545b3e497155bff9f75b97
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-02 05:05:48 +0530

    break and continue

--- iterators_05/01_for.js
+++ iterators_05/01_for.js
@@ -17,9 +17,20 @@ for (let i = 0; i < 10; i++) {
    console.log(element);
 } */
 
-for (let i = 0; i < 10; i++) {
+/* for (let i = 1; i < 10; i++) {
    console.log(`Outer loop : ${i}`);
-   for (let j = 0; j < 10; j++) {
-      console.log(`Inner loop : ${j} and inner loop ${i}`);
+   for (let j = 1; j < 10; j++) {
+      //console.log(`Inner loop : ${j} and inner loop ${i}`);
+      console.log(` ${i} * ${j} = ${i * j}`);
    }
-}
+} */
+
+/* let myArr = ["flash", "batman", "superman"];
+console.log(myArr.length);
+
+for (let i = 0; i < myArr.length; i++) {
+   const element = myArr[i];
+   console.log(element);
+} */
+
+// break and continue
