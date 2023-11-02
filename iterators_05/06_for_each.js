9e5cc4e87bb8ba99c589d95bfae04805135e391c
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-03 04:53:13 +0530

    update

---
+++ iterators_05/06_for_each.js
@@ -0,0 +1,38 @@
+// for each => A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+
+const coding = ["js", "rb", "py", "java", "cpp"];
+
+coding.forEach(function (value) {
+   console.log(value);
+});
+
+coding.forEach((value) => {
+   console.log(value);
+});
+
+function printMe(item) {
+   console.log(item);
+}
+coding.forEach(printMe);
+
+coding.forEach((item, index, arr) => {
+   console.log(item, index, arr);
+});
+
+const myCoding = [
+   {
+      languageName: "javascript",
+      languageFileName: "js",
+   },
+   {
+      languageName: "java",
+      languageFileName: "java",
+   },
+   {
+      languageName: "python",
+      languageFileName: "py",
+   },
+];
+myCoding.forEach((item) => {
+   console.log(item.languageName);
+});
