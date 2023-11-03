83eb3f18342d6b82d2dcf738a23a02477d1d5734
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-04 04:35:12 +0530

    notes updates

--- iterators_05/07_filter.js
+++ iterators_05/07_filter.js
@@ -1,5 +1,63 @@
+// filter => Returns the elements of an array that meet the condition specified in a callback function.
+
 const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const newNum = myNum.filter((num) => num > 4);
 
-console.log(newNum);
+/* const newNum = myNum.filter((num) => {
+   return num > 4;
+}); */
+console.log(newNum); //[5, 6, 7, 8, 9, 10];
+
+
+const newNum = [];
+
+myNum.forEach((num) => {
+   if (num > 4) {
+      newNum.push(num);
+   }
+});
+console.log(newNum); //[ 5, 6, 7, 8, 9, 10 ]
+
+const books = [
+   {
+      title: "Book One",
+      genre: "Fiction",
+      publish: 1981,
+   },
+   {
+      title: "Book Two",
+      genre: "Non-Fiction",
+      publish: 1992,
+   },
+   {
+      title: "Book Three",
+      genre: "History",
+      publish: 1981,
+   },
+   {
+      title: "Book Four",
+      genre: "Non-Fiction",
+      publish: 1999,
+   },
+   {
+      title: "Book Five",
+      genre: "Science",
+      publish: 1989,
+   },
+   {
+      title: "Book Six",
+      genre: "Fiction",
+      publish: 2000,
+   },
+];
+
+const userBooks = books.filter((bk) => bk.genre === "History");
+
+console.log(userBooks); //[ { title: 'Book Three', genre: 'History', publish: 1981 } ]
+
+/* const userBooks = books.filter((bk) => {
+   return bk.publish >= 1995 && bk.genre === "Fiction";
+}); */
+
+console.log(userBooks); // [ { title: 'Book Six', genre: 'Fiction', publish: 2000 } ]
