72a906a4059a1c307b935e4c0fd7bbcec1f993b7
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-07 06:24:15 +0530

    firstElementChild

---
+++ 06_dom/02_new_events/script.js
@@ -0,0 +1,13 @@
+const parent = document.querySelector(".parent");
+console.log(parent);
+console.log(parent.children);
+console.log(parent.children[1].innerHTML); 
+
+for (let i = 0; i < parent.children; i++) {
+   console.log(parent.children[i].innerHTML);
+}
+// firstElementChild
+ parent.children[1].style.color = "orange";
+console.log(parent.firstElementChild);
+
+
