b8e5c19506b6cadbf7408b294a3494b4ba7be12b
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-11-07 06:35:43 +0530

    notes update

--- 06_dom/02_new_events/script.js
+++ 06_dom/02_new_events/script.js
@@ -1,14 +1,35 @@
 const parent = document.querySelector(".parent");
 console.log(parent);
+// <div class="parent">…</div>
+
 console.log(parent.children);
-console.log(parent.children[1].innerHTML); 
+// HTMLCollection(6) [div.day, div.day, div.day, div.day, div.day, div.day]
+
+console.log(parent.children[1].innerHTML);
+// Tuesday
 
 for (let i = 0; i < parent.children; i++) {
    console.log(parent.children[i].innerHTML);
 }
+
 // firstElementChild
- parent.children[1].style.color = "orange";
+parent.children[1].style.color = "orange";
 console.log(parent.firstElementChild);
+// <div class="day">Monday</div>
+
 // lastElementChild
 console.log(parent.lastElementChild);
+//<div class="day">Saturday</div>
 
+const dayOne = document.querySelector(".day");
+console.log(dayOne);
+// <div class="day">Monday</div>
+
+console.log(dayOne.parentElement);
+// <div class="parent">…</div>
+
+console.log(dayOne.nextElementSibling);
+// <div class="day" style="color: orange;">Tuesday</div>
+
+console.log("NODES: ", parent.childNodes);
+// NODES:  NodeList(13) [text, div.day, text, div.day, text, div.day, text, div.day, text, div.day, text, div.day, text]
